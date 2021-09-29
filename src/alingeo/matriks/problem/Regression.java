34d35b1e933a70fdef447d8f496e995b439b584e
vito <vitosgh@gmail.com>
2021-09-29 10:16:58 +0700

    Finished regression and added Regression test case

--- src/alingeo/matriks/problem/Regression.java
+++ src/alingeo/matriks/problem/Regression.java
@@ -6,6 +6,9 @@
 package alingeo.matriks.problem;
 
 import alingeo.matriks.Matrix;
+import alingeo.matriks.solver.SPLSolver;
+import alingeo.matriks.solver.SPLSolver.SolutionResult;
+import alingeo.matriks.solver.SPLSolver.SolutionResult.SolutionType;
 
 /**
  *
@@ -15,11 +18,12 @@ public class Regression {
     
     private double intercept;
     private double[] coef;
-    private int noCoef;
+    private boolean hasSolution;
     
     public Regression(){
         this.intercept = 0;
         this.coef = null;
+        this.hasSolution = false;
     }
     
     public double getIntercept(){
@@ -28,6 +32,10 @@ public class Regression {
     
     public double[] getCoef(){
         return this.coef;
+    }
+    
+    public boolean hasSolution(){
+        return this.hasSolution;
     }
     
     public void fit(Matrix m){
@@ -46,15 +54,16 @@ public class Regression {
         [ 1  x1m x2m x3m ... xnm ]
         
         Persamaan Normal: y = A*b
-        Akan dihitung: ((A^T)*A)^-1 *(A^T)*y untuk mendapatkan b
+        Akan dihitung: ((A^T)*A)^-1) = y untuk mendapatkan b
         */
-        Matrix matA, matAT, matY;
+        Matrix matA, matAT, matATCpy, matY, matB;
         
         matY = new Matrix(m.getNRow(), 1);
         
         matA = m.copy();
         matAT = Matrix.transpose(matA);
         
+        // matY sebagai RHS
         for (int i=0; i < matY.getNRow(); i++){
             matY.setElmt(i, 0, matA.getElmt(i, matA.getNCol()-1));
         }
@@ -69,16 +78,46 @@ public class Regression {
         for (int i = (nRow-1); i > 0; i--){
             matAT.RowSwap(i, i-1);
         }
-        
+        matATCpy = matAT.copy();
         matA = Matrix.transpose(matAT);
         
-//        matAT.mul(matA);
-        System.out.println(matA.toString());
-        System.out.println();
-        System.out.println(matAT.toString());
+        // matAT dan matA sebagai LHS
+        matAT.mul(matA);
+        
+        // matATCpy sebagai RHS
+        matATCpy.mul(matY);
+        
+        matB = matAT.copy();
+        matB.setNCol(matB.getNCol()+1);
+        
+        for (int i=0; i < matB.getNRow(); i++){
+            matB.setElmt(i, matB.getNCol()-1, matATCpy.getElmt(i, 0));
+        }
+        SolutionResult result = SPLSolver.gaussJordanMethod(matB);
+        if (result.getType() == SolutionType.UNIQUE){
+            this.hasSolution = true;
+            
+            Matrix res = result.getResult();
+            this.intercept = res.getElmt(0, 0);
+            this.coef = new double[res.getNRow()-1];
+            for (int i = 1; i < res.getNRow(); i++){
+                this.coef[i-1] = res.getElmt(i, 0);
+            }
+        }
+        
     }
     
-    public void predict(){
+    public double predict(double[] x){
+        if ((!this.hasSolution) || (x.length != this.coef.length)){
+            return Double.NaN;
+        }
+        double y_hat = 0;
+        y_hat += this.intercept;
         
+        for (int i = 0; i < x.length; i++){
+            y_hat += this.coef[i] * x[i];
+        }
+        
+        return y_hat;
     }
 }
--- test/alingeo/matriks/problem/RegressionTest.java
+++ test/alingeo/matriks/problem/RegressionTest.java
@@ -6,6 +6,7 @@
 package alingeo.matriks.problem;
 
 import alingeo.matriks.Matrix;
+import alingeo.matriks.solver.SPLSolver;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,34 +18,73 @@ public class RegressionTest {
     
     public RegressionTest() {
     }
+    
+    // Data source: https://www.statology.org/multiple-linear-regression-by-hand/
+    Matrix tc1 = new Matrix(new double[][]{
+        {60, 22, 140},
+        {62, 25, 155},
+        {67, 24, 159},
+        {70, 20, 179},
+        {71, 15, 192},
+        {72, 14, 200},
+        {75, 14, 212},
+        {78, 11, 215}
+    });
+    
+    // Subsample pada "studi kasus regresi linier berganda"
+    Matrix tc2 = new Matrix(new double[][]{
+        {72.4, 76.3, 29.18, 0.90},
+        {41.6, 70.3, 29.35, 0.91},
+        {34.3, 77.1, 29.24, 0.96},
+        {35.1, 68.0, 29.27, 0.89},
+        {10.7, 79.0, 29.78, 1.00},
+        {12.9, 67.4, 29.39, 1.10},
+        {8.30, 66.8, 29.69, 1.15},
+        {20.1, 76.9, 29.48, 1.03},
+        {72.2, 77.7, 29.09, 0.77},
+        {24.0, 67.7, 29.60, 1.07},
+        {23.2, 76.8, 29.38, 1.07},
+        {47.4, 86.6, 29.35, 0.94},
+        {31.5, 76.9, 29.63, 1.10},
+        {10.6, 86.3, 29.56, 1.10},
+        {11.2, 86.0, 29.48, 1.10},
+        {73.3, 76.3, 29.40, 0.91},
+        {75.4, 77.9, 29.28, 0.87},
+        {96.6, 78.7, 29.29, 0.78},
+        {107.4,86.8, 29.03, 0.82},
+        {54.9, 70.9, 29.37, 0.95}
+    });
 
     /**
      * Test of getIntercept method, of class Regression.
      */
-//    @Test
-//    public void testGetIntercept() {
-//        System.out.println("getIntercept");
-//        Regression instance = new Regression();
-//        double expResult = 0.0;
-//        double result = instance.getIntercept();
-//        assertEquals(expResult, result, 0.0);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of getCoef method, of class Regression.
-//     */
-//    @Test
-//    public void testGetCoef() {
-//        System.out.println("getCoef");
-//        Regression instance = new Regression();
-//        double[] expResult = null;
-//        double[] result = instance.getCoef();
-//        assertArrayEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+    @Test
+    public void testGetIntercept() {
+        System.out.println("getIntercept");
+        Regression instance = new Regression();
+
+        double result = instance.getIntercept();
+        assertEquals(0.0, result);
+        
+        instance.fit(tc1);
+        assertEquals(-6.867487247726643, instance.getIntercept());
+    }
+
+    /**
+     * Test of getCoef method, of class Regression.
+     */
+    @Test
+    public void testGetCoef() {
+        System.out.println("getCoef");
+        Regression instance = new Regression();
+        double[] expResult = null;
+        double[] result = instance.getCoef();
+        assertArrayEquals(expResult, result);
+        
+        instance.fit(tc1);
+        expResult = new double[]{3.1478931026835206, -1.6561432690175215};
+        assertArrayEquals(expResult, instance.getCoef());
+    }
 
     /**
      * Test of fit method, of class Regression.
@@ -52,20 +92,10 @@ public class RegressionTest {
     @Test
     public void testFit() {
         System.out.println("fit");
-        Matrix m = new Matrix(new double[][]{
-            {60, 22, 140},
-            {62, 25, 155},
-            {67, 24, 159},
-            {70, 20, 179},
-            {71, 15, 192},
-            {72, 14, 200},
-            {75, 14, 212},
-            {78, 11, 215}
-        });
-        Regression instance = new Regression();
-        instance.fit(m);
-        // TODO review the generated test code and remove the default call to fail.
-        assertEquals(false, true);
+        
+        Regression ins = new Regression();
+        ins.fit(tc1);
+        ins.fit(tc2);
     }
 
     /**
@@ -74,10 +104,32 @@ public class RegressionTest {
     @Test
     public void testPredict() {
         System.out.println("predict");
+        Regression ins = new Regression();
+        double[] x = new double[]{68, 18};
+        
+        assertEquals(true, Double.isNaN(ins.predict(x)));
+        
+        ins.fit(tc1);
+        assertEquals(177.37866489243737, ins.predict(x));
+        
+        ins.fit(tc2);
+        x = new double[]{50, 76, 29.30};
+        assertEquals(0.938434226221665, ins.predict(x));
+    }
+
+    /**
+     * Test of hasSolution method, of class Regression.
+     */
+    @Test
+    public void testHasSolution() {
+        System.out.println("hasSolution");
         Regression instance = new Regression();
-        instance.predict();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        boolean result = instance.hasSolution();
+        
+        assertEquals(false, result);
+        
+        instance.fit(tc1);
+        assertEquals(true, instance.hasSolution());
     }
     
 }
